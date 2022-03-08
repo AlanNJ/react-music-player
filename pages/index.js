7d46fb6b2972d6947bc518f5576d4a523baaeda7
AlanNJ <alannpn75@gmail.com>
2022-03-08 23:18:45 +0530

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Main from "../components/Main";
 
 export const index = () => {
+	const [music, setMusic] = useState("");
 	return (
 		<div className="container">
 			<Main />
